c64b0234d4f6a8b8770c6eaff98c353c623524e3
Alone Mpitula <Alonemapitlula@gmail.com>
2024-08-23 18:38:59 +0200

    Initial commit

--- Nwu_Tech_Trends_TelemetryPortal_MVC/TelemetryPortal_MVC/obj/Release/net8.0/TelemetryPortal_MVC.AssemblyInfo.cs
+++ Nwu_Tech_Trends_TelemetryPortal_MVC/TelemetryPortal_MVC/obj/Release/net8.0/TelemetryPortal_MVC.AssemblyInfo.cs
@@ -16,7 +16,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelemetryPortal_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d75b51fc7fcd363518bef367a1eb421832d2cd")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0dbe0979756763b0f71aad058e4eb15bab51754")]
+>>>>>>> Initial commit
 [assembly: System.Reflection.AssemblyProductAttribute("TelemetryPortal_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelemetryPortal_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
