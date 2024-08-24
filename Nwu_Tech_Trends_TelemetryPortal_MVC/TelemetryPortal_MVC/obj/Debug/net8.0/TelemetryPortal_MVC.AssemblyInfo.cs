80cb37c729ef2dbb2114b36ac9520513c08408c8
Alone Mpitula <Alonemapitlula@gmail.com>
2024-08-24 15:19:51 +0200

    Last commit

--- Nwu_Tech_Trends_TelemetryPortal_MVC/TelemetryPortal_MVC/obj/Debug/net8.0/TelemetryPortal_MVC.AssemblyInfo.cs
+++ Nwu_Tech_Trends_TelemetryPortal_MVC/TelemetryPortal_MVC/obj/Debug/net8.0/TelemetryPortal_MVC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelemetryPortal_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df79ac1154664fa6d8782b25dc54ac2d5ed6103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b6158055ae46db2531e8a4a7b0e94d5aa28642")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelemetryPortal_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelemetryPortal_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
